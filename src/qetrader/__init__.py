99526e7ad8e40a1a19e05c7c4274d9383ce12851
scott2011 <82897735+scott2011@users.noreply.github.com>
2023-10-08 20:47:18 +0800

    1.0.32：TA prodTime bug：

--- src/qetrader/__init__.py
+++ src/qetrader/__init__.py
@@ -5,7 +5,7 @@ Created on Tue Nov 30 20:01:48 2021
 @author: ScottStation
 """
 
-__version__ = '1.0.31'
+__version__ = '1.0.32'
 
 from .qeinterface import qeStratBase
 from .qeinterface import make_order
